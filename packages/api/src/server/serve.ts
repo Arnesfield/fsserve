1b72c7093fcef9b15849f840ab38e004fbe9989a
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-12-28 10:53:43 +0800

    Use promise for listen

--- packages/api/src/server/serve.ts
+++ packages/api/src/server/serve.ts
@@ -17,40 +17,33 @@ export async function serve(options: ServeOptions = {}): Promise<Server> {
     throw new Error('Port should be a number >= 0 and < 65536.');
   }
   const fastify = await createServer(options);
-  fastify.listen({ port, host: '0.0.0.0' }, (error, address) => {
-    if (error) {
-      console.error('%s:', error.name, error.message);
-      process.exitCode = 1;
-      return;
-    }
-    const target =
-      path.relative('', path.resolve(options.rootDir || '')) || '.';
-    console.log(chalk`{yellow Serving}     {cyan %s/}`, target);
-    // operations
-    const o = options.operations || {};
-    const operations: { allow?: boolean; name: string }[] = [
-      { allow: o.download, name: 'Download' },
-      { allow: o.remove, name: 'Remove' },
-      { allow: o.upload, name: 'Upload' },
-      { allow: o.modify, name: 'Modify' }
-    ];
-    console.log(
-      chalk`{yellow Operations}  %s  %s  %s  %s`,
-      ...operations.map(operation => {
-        const prefix = operation.allow ? 'green \u2714' : 'red \u2718';
-        return chalk`{${prefix}} ${operation.name}`;
-      })
-    );
-    // addresses
-    const url = new URL(address);
-    const addressesLabel = 'Addresses';
-    const spaces = ' '.repeat(addressesLabel.length + 1);
-    getAddresses().forEach((address, index) => {
-      url.hostname = address;
-      const label = index > 0 ? spaces : chalk`{yellow ${addressesLabel}} `;
-      console.log(chalk`%s  %s`, label, url.toString());
-    });
-    console.log(chalk`\nPress {cyan Ctrl+C} to {red stop}.`);
+  const address = await fastify.listen({ port, host: '0.0.0.0' });
+  const target = path.relative('', path.resolve(options.rootDir || '')) || '.';
+  console.log(chalk`{yellow Serving}     {cyan %s/}`, target);
+  // operations
+  const o = options.operations || {};
+  const operations: { allow?: boolean; name: string }[] = [
+    { allow: o.download, name: 'Download' },
+    { allow: o.remove, name: 'Remove' },
+    { allow: o.upload, name: 'Upload' },
+    { allow: o.modify, name: 'Modify' }
+  ];
+  console.log(
+    chalk`{yellow Operations}  %s  %s  %s  %s`,
+    ...operations.map(operation => {
+      const prefix = operation.allow ? 'green \u2714' : 'red \u2718';
+      return chalk`{${prefix}} ${operation.name}`;
+    })
+  );
+  // addresses
+  const url = new URL(address);
+  const addressesLabel = 'Addresses';
+  const spaces = ' '.repeat(addressesLabel.length + 1);
+  getAddresses().forEach((address, index) => {
+    url.hostname = address;
+    const label = index > 0 ? spaces : chalk`{yellow ${addressesLabel}} `;
+    console.log(chalk`%s  %s`, label, url.toString());
   });
+  console.log(chalk`\nPress {cyan Ctrl+C} to {red stop}.`);
   return fastify.server;
 }
