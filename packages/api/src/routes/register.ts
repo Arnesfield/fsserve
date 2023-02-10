0da30bec3e983b81f7a685da8fd296d1ce57c395
Jefferson Rylee <rylee.jeff385@gmail.com>
2023-02-10 10:55:16 +0800

    Serve web build

--- packages/api/src/routes/register.ts
+++ packages/api/src/routes/register.ts
@@ -1,3 +1,6 @@
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import {
   description,
   homepage,
@@ -5,13 +8,15 @@ import {
   name,
   version
 } from '../../../../package.json';
+import { FsError } from '../core/error';
 import { FsServePluginCallback } from '../types/plugin.types';
 import { fileRoutes } from './file.routes';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // entry point for server routes
 
 export const register: FsServePluginCallback = (fastify, options) => {
-  // TODO: serve web build
   const url = new URL('http://localhost:8082');
   const allowedOrigins = options.ctx.addresses.map(address => {
     url.hostname = address;
@@ -22,13 +27,28 @@ export const register: FsServePluginCallback = (fastify, options) => {
     const origin = request.headers.origin;
     if (origin && allowedOrigins.includes(origin)) {
       reply.header('Access-Control-Allow-Origin', origin);
+      reply.header('Access-Control-Allow-Methods', 'GET, POST');
     }
-    reply.header('Access-Control-Allow-Methods', 'GET, POST');
     done();
   });
-  fastify.get('/api', () => {
+  // serve web build
+  const API_PATH = '/api';
+  fastify.register(fastifyStatic, { root: path.join(__dirname, '../dist') });
+  fastify.setNotFoundHandler((request, reply) => {
+    if (request.url.startsWith(API_PATH)) {
+      reply.status(404);
+      reply.send(new FsError(404, `Cannot ${request.method} ${request.url}`));
+    } else {
+      reply.status(200).sendFile('index.html');
+    }
+  });
+  // serve api
+  fastify.get(API_PATH, () => {
     return { name, version, description, homepage, license };
   });
-  fastify.register(fileRoutes, { prefix: '/api/files', ctx: options.ctx });
+  fastify.register(fileRoutes, {
+    prefix: `${API_PATH}/files`,
+    ctx: options.ctx
+  });
   return fastify;
 };
